998c3901123038aee84f5113e9cff635aff4de1a
bookkuun <bookkuun@gmail.com>
2025-01-04 22:05:32 +0900

    add: importの設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: path.resolve(__dirname, "./src"),
+      },
+      {
+        find: "@apis",
+        replacement: path.resolve(__dirname, "./src/apis"),
+      },
+      {
+        find: "@components",
+        replacement: path.resolve(__dirname, "./src/components"),
+      },
+      {
+        find: "@features",
+        replacement: path.resolve(__dirname, "./src/features"),
+      },
+      {
+        find: "@hooks",
+        replacement: path.resolve(__dirname, "./src/hooks"),
+      },
+      {
+        find: "@redux",
+        replacement: path.resolve(__dirname, "./src/redux"),
+      },
+      {
+        find: "@routes",
+        replacement: path.resolve(__dirname, "./src/routes"),
+      },
+    ],
+  },
+});
