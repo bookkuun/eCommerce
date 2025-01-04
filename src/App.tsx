ae465fb3883ee4067ccb19d92cc94c9a9da1b6fe
bookkuun <bookkuun@gmail.com>
2025-01-04 21:38:12 +0900

    add: react-router-domの使い方の確認

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-
 function App() {
-  return (
-    <>
-      <Header />
-      <div>App</div>
-      <Footer />
-    </>
-  );
+  return <>App</>;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,23 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 // Import the App component
-import App from "./App.tsx";
+import UserRoutes from "./routes/user.routes.tsx";
+import ProductList from "./features/product/components/ProductList.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <UserRoutes />,
+    children: [
+      {
+        path: "products",
+        element: <ProductList />,
+      },
+      {
+        path: "profile",
+        element: <div>User Profile</div>,
+      },
+    ],
   },
 ]);
 
